16f3fb76c0f43c855116ba68519ebe16cc352a01
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-04-16 20:20:33 +0200

    refactor: get filename from query

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,15 @@ const minioClient = new Client({
 });
 
 app.get('/dsvc/presignedUrl', (req, res) => {
+    const fileName = req.query.fileName as string;
+    if (!fileName) {
+        res.status(400).send("Missing fileName");
+        return;
+    }
+
     minioClient.presignedGetObject(
         process.env.MINIO_BUCKET!,
-        req.body.filename,
+        fileName,
         10 * 60 /* 10 minutes */,
         (err, presignedUrl) => {
             if (err) {
