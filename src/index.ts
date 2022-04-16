2f6d73352f6cd5f1df70a05ba3e765de939f82d2
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-04-16 20:08:36 +0200

    feat: add /presignedUrl request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import { Client } from "minio";
 
 dotenv.config({ path: __dirname + '/../.env' });
 
@@ -7,3 +8,46 @@ const app = express();
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use('/dsvc/healthcheck', require('express-healthcheck')());
+
+const minioClient = new Client({
+    endPoint: process.env.MINIO_ENDPOINT!,
+    port: parseInt((process.env.MINIO_PORT) ? process.env.MINIO_PORT : "9000", 10),
+    useSSL: (process.env.MINIO_SECURE === 'true'),
+    accessKey: process.env.MINIO_ACCESSKEY!,
+    secretKey: process.env.MINIO_SECRETKEY!
+});
+
+app.get('/dsvc/presignedUrl', (req, res) => {
+    minioClient.presignedGetObject(
+        process.env.MINIO_BUCKET!,
+        req.body.filename,
+        10 * 60 /* 10 minutes */,
+        (err, presignedUrl) => {
+            if (err) {
+                res.status(500);
+                res.send({
+                    error: err.toString()
+                });
+            } else {
+                res.status(200);
+                res.send({
+                    url: presignedUrl
+                });
+            }
+            res.end();
+        })
+});
+
+minioClient.bucketExists(process.env.MINIO_BUCKET!, function (error: any, exists: boolean) {
+    if (error) {
+        return console.log(error);
+    }
+
+    if (exists) {
+        app.listen(process.env.PORT || 8080, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8080}`);
+        });
+    } else {
+        return console.log(`Bucket "${process.env.MINIO_BUCKET}" doesn't exist! Please change the configuration or create the bucket.`);
+    }
+});
